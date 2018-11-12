babfa31a62fc41ac143a0750a666d184a0a9ba75
Ellis <ellis.king15@imperial.ac.uk>
2018-11-12 11:00:41 +0000

    Update aip.py

--- aip.py
+++ aip.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 from astropy.io import fits
 from statistics import mode
 import matplotlib.pyplot as plt
@@ -19,8 +13,21 @@ unique = []
 [unique.append(item) for item in data_2 if item not in unique]
 
 def mask(data=data):
+    "edges of image are masked first (thickness = 116)"
+    data[0:116,0:data.shape[1]] = 0
+    data[116:data.shape[0], 0:116] = 0
+    data[(data.shape[0]-116):data.shape[0], 116:data.shape[1]] = 0
+    data[116:(data.shape[0]-116), (data.shape[1]-116):data.shape[1]] = 0
+    "masked sources"
     data[657,2530]=0
     circle(3443,2562,2)
+    circle(3600,2535,3)
+    data[4548,1054]=0
+    circle(206,1433,37)
+    circle(2261,904,23)
+    circle(2286,905,30)
+    circle(1495,634,21)
+    circle(3296,775,10)
     
 def histogram(data_2):
     plt.xlim(3300,3550)
@@ -30,9 +37,14 @@ def histogram(data_2):
 
 bkgd = np.mean(data_2)
 
-def max_pixel(data): 
+def max_pixel(data=data): 
     max_pix_index = np.unravel_index(data.argmax(), data.shape)
+    if data.argmax() >= 3623.5:
+        print("All is well")
+    else:
+        print("THIS DOES NOT COUNT")
     return max_pix_index
+
 
 def magnitude(x,y,radius,localbkgd = bkgd, ZPinst=ZPinst):
     total_intensity = 0
@@ -40,9 +52,19 @@ def magnitude(x,y,radius,localbkgd = bkgd, ZPinst=ZPinst):
     cx, cy = x,y 
     x,y = np.ogrid[-radius: radius+1, -radius: radius+1]
     index = x**2 + y**2 <= radius**2
-    for i in data[cx-radius:cx+radius+1, cy-radius:cy+radius+1][index]:
+    if cx < data.shape[0]/2 and cx < radius:
+        cx = radius
+    if cy < data.shape[1]/2 and cy < radius:
+        cy = radius
+    if cx > data.shape[0]/2 and (data.shape[0] - cx) < radius:
+        cx = data.shape[0] - radius - 1
+    if cy > data.shape[1]/2 and (data.shape[1] - cy) < radius:
+        cy = data.shape[1] - radius - 1
+    area = data[cx-radius:cx+radius+1, cy-radius:cy+radius+1][index]
+    for i in area:
         total_intensity += i
-        numberofpoints += 1
+        if i != 0:
+            numberofpoints += 1
 
     net_intensity = total_intensity - localbkgd*numberofpoints
     mag = -2.5*np.log10(net_intensity)
@@ -53,8 +75,16 @@ def circle(x,y,radius, data=data):
     cx, cy = x,y
     y,x = np.ogrid[-radius: radius+1, -radius: radius+1]
     index = x**2 + y**2 <= radius**2
+    if cx < data.shape[0]/2 and cx < radius:
+        cx = radius
+    if cy < data.shape[1]/2 and cy < radius:
+        cy = radius
+    if cx > data.shape[0]/2 and (data.shape[0] - cx) < radius:
+        cx = data.shape[0] - radius - 1
+    if cy > data.shape[1]/2 and (data.shape[1] - cy) < radius:
+        cy = data.shape[1] - radius - 1
     data[cx-radius:cx+radius+1, cy-radius:cy+radius+1][index] = 0
-    
+        
 
 def local_background(x, y):
    max_value = data[x,y]
@@ -71,9 +101,40 @@ def local_background(x, y):
        radius +=1                
        means = np.mean((data[x, y +  radius],data[(x + radius),y],data[x,y - (radius)],data[x - (radius ),y]))
        meanpoints.append(means)
-             
+
    return radius, meanpoints[-1], meanpoints[0]
 
+def realcentre(x,y, bkgd = bkgd):
+    yplus = 0
+    yminus = 0
+    xplus = 0
+    xminus =0
+    while data[x, y + yplus] > bkgd:
+        yplus +=1
+    while data[x,y - yminus] > bkgd:
+        yminus +=1
+    while data[x + xplus, y] > bkgd:
+        xplus +=1
+    while data[x - xminus,y] > bkgd:
+        xminus += 1
+    
+    xchange = (xplus - xminus)/2
+    ychange = (yplus - yminus)/2
+
+    newx = int(x+xchange)
+    newy = int(y+ychange)
+    
+    return newx,newy, (xplus+xminus)/2, (yplus+yminus)/2
+
+def edgethickness(x, y,axis, value = 3421):
+    i=0
+    if axis == 1:
+        while data[y, x-i] == value:
+            i += 1
+    if axis == 2:
+        while data[y+i, x] == value:
+            i += 1   
+    return i
 
 
 
@@ -81,6 +142,8 @@ def local_background(x, y):
 #plt.figure()
 #plt.imshow(image_data, cmap='gray')
 #plt.colorbar()
+
+
 
 
 
